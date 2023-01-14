ad42d3576e216e215752aa433f4e02361639c7e5
Sadman26 <fareless.ranger@gmail.com>
2023-01-14 21:28:33 +0600

    Some Line edik shedik

--- app.py
+++ app.py
@@ -2,10 +2,7 @@ import psycopg2
 connection=psycopg2.connect(user="postgres",password="sad@2600",port="5432",database="bubtlab")
 connection.autocommit=True
 cursor=connection.cursor()
-sql2 = '''copy omg
-from 'E:\DDB\data.csv'
-delimiter ','
-csv header;'''
-cursor.execute(sql2)
+sql = '''copy omg from 'E:\DDB\data.csv' delimiter ',' csv header;'''
+cursor.execute(sql)
 connection.commit()
 cursor.close()
